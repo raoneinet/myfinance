3e3c85200bb314b8ede9767caecf2286a8f9a7c9
raoneinet <raoneinet@hotmail.com>
2025-08-21 01:04:46 +0100

    creating user

--- src/views/signup/signup.tsx
+++ src/views/signup/signup.tsx
@@ -1,11 +1,13 @@
-import {auth} from "@/app/fisebase"
+
 import {SignupForm} from "./signupForm"
 
+
 export const Signup = ()=>{
+
     return (
         <div className="w-full h-full flex justify-center items-center p-2 bg-gray-100">
             <div className="flex justify-center items-center p-5 w-2xl h-80 bg-white rounded-lg shadow shadow-gray-400">
-                <SignupForm/>
+                <SignupForm />
             </div>
         </div>
     )
