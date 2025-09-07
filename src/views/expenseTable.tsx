dc60940f36d351f0a2c224649fe9bfb721739f82
raoneinet <raoneinet@hotmail.com>
2025-09-07 17:05:51 +0100

    fix: fixing tables with new components

--- src/views/expenseTable.tsx
+++ src/views/expenseTable.tsx
@@ -3,26 +3,15 @@ import { useState, useEffect } from "react"
 import api from "@/app/api/api"
 import { FinanceType } from "@/types/financeTypes"
 import { FinanceTable } from "@/components/financeTable"
-export const ExpenseTable = () => {
 
-    const [finance, setFinance] = useState<FinanceType[]>([]);
+type Props = {
+    finance: FinanceType[]
+    getFinance: ()=>void
+}
+
+export const ExpenseTable = ({finance, getFinance}: Props) => {
 
     useEffect(() => {
-        const getFinance = async () => {
-            try {
-                const res = await api.get("/finance.php")
-                console.log("Finanças: ", res)
-
-                if (res.status !== 200) throw new Error("Erro ao buscar finanças")
-
-                const data = res.data.finance
-
-                setFinance(data)
-            } catch (error: any) {
-                console.log("Erro ao buscar por finanças: ", error)
-            }
-        }
-
         getFinance()
     }, [])
 
