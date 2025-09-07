6b59a69c146a4b86ae1d0f039d5bf6abfeb5fd29
raoneinet <raoneinet@hotmail.com>
2025-09-07 17:06:20 +0100

    fix: fixing components

--- src/views/homedashboard.tsx
+++ src/views/homedashboard.tsx
@@ -1,33 +1,58 @@
-import {useState} from "react"
+import { useState } from "react"
 import { TotalFinancing } from "./totalFinancing"
 import { ExpenseTable } from "./expenseTable"
-import {InsertExpense} from "./insertExpense"
+import { InsertExpense } from "./insertExpense"
 import { SearchExpense } from "./searchFinance"
+import { FinanceType } from "@/types/financeTypes"
+import api from "@/app/api/api"
 
 export const HomeDashboard = () => {
 
     const [showModal, setShowModal] = useState(false)
+    const [finance, setFinance] = useState<FinanceType[]>([]);
 
-    const handleShowModal = ()=>{
+    const handleShowModal = () => {
         setShowModal(true)
     }
 
-    const handleCloseModal = ()=>{
+    const handleCloseModal = () => {
         setShowModal(false)
+    }
+
+    const getFinance = async () => {
+        try {
+            const res = await api.get("/finance.php")
+            console.log("Finanças: ", res)
+
+            if (res.status !== 200) throw new Error("Erro ao buscar finanças")
+
+            const data = res.data.finance
+
+            setFinance(data)
+        } catch (error: any) {
+            console.log("Erro ao buscar por finanças: ", error)
+        }
     }
 
     return (
         <>
             <div className="container mx-auto h-full">
                 <TotalFinancing />
-                <SearchExpense 
+                <SearchExpense
                     setModal={handleShowModal}
+
                 />
                 {showModal &&
-                    <InsertExpense closeModal={handleCloseModal}/>
+                    <InsertExpense
+                        closeModal={handleCloseModal}
+                        updateDashboard={getFinance} 
+                    />
                 }
-                
-                <ExpenseTable />
+
+                <ExpenseTable
+                    finance={finance}
+                    getFinance={getFinance}
+                />
             </div>
         </>
     )
