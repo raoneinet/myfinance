e3768bdc67f6ddab8b842e6940773825e03910ec
raoneinet <raoneinet@hotmail.com>
2025-09-13 11:09:22 +0100

    fix: adding style to icons and  text in action modal

--- src/components/itemActionBox.tsx
+++ src/components/itemActionBox.tsx
@@ -1,12 +1,12 @@
 export const ItemActionBox = ()=>{
     return (
-        <div className="w-fit shadow shadow-gray-400 px-4 py-2 rounded-md mt-2 absolute bg-white">
-            <div>
-                <img/>
+        <div className="flex flex-col justify-center w-fit shadow shadow-gray-400 px-4 py-3 rounded-md mt-2 absolute bg-white">
+            <div className="flex gap-2 cursor-pointer font-semibold">
+                <img src="/assets/icons/edit_icon.png" className="w-4 h-4"/>
                 Editar
             </div>
-            <div>
-                <img />
+            <div className="flex gap-2 cursor-pointer font-semibold">
+                <img src="/assets/icons/delete_icon.png" className="w-4 h-4"/>
                 Apagar
             </div>
         </div>
