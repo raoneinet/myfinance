1ba9ea830e33a5739f6b6a4d7e8b18fb33190c77
raoneinet <raoneinet@hotmail.com>
2025-10-04 13:08:04 +0100

    fix: using button pattern

--- src/components/allFinaneBtn.tsx
+++ src/components/allFinaneBtn.tsx
@@ -2,10 +2,12 @@ export const AllFinanceBtn = ({allFinance}: {allFinance: ()=>void}) => {
 
     const getAllFinance = ()=> allFinance()
     return (
-        <input
+        <button
             type="button"
             onClick={getAllFinance}
-            className="w-full px-3 py-2 bg-gray-50 hover:bg-gray-300 text-center md:w-fit rounded-xl text-gray-600 cursor-pointer outline-0"
-            value="Todas as transações" />
+            className="px-3 py-2 bg-gray-50 hover:bg-gray-300 text-center md:w-fit rounded-xl text-gray-600 cursor-pointer outline-0"
+        >
+                Todas as transações
+        </button>
     )
 }
--- src/components/currentFinanceBtn.tsx
+++ src/components/currentFinanceBtn.tsx
@@ -2,11 +2,13 @@
 export const CurrentFinanceBtn = ({currentFinance}: {currentFinance: ()=>void}) => {
     const getCurrentFinance = ()=> currentFinance() 
     return (
-        <input
+        <button
             type="button"
             onClick={getCurrentFinance}
-            className="w-full px-3 py-2 bg-gray-50 hover:bg-gray-300 text-center md:w-fit rounded-xl text-gray-600 cursor-pointer outline-0"
-            value="Atual" />
+            className="px-3 py-2 bg-gray-50 hover:bg-gray-300 text-center md:w-fit rounded-xl text-gray-600 cursor-pointer outline-0"
+        >
+            Atual
+        </button>
     )
 }
 
