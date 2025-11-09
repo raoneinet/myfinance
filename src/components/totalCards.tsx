fdda397ce44a18eeb8959669dfedfa63fa3627f1
raoneinet <raoneinet@hotmail.com>
2025-11-09 11:02:48 +0000

    fix: totalCards

--- src/components/totalCards.tsx
+++ src/components/totalCards.tsx
@@ -10,7 +10,7 @@ type Props = {
 export const TotalCards = ({ title, value, bgColor, icon, borderColor }: Props) => {
     return (
         <div className={`bg-neutral-800 rounded-lg shadow border ${borderColor}`}>
-            <div className="flex flex-col justify-between px-3 py-5 text-lg gap-2">
+            <div className="flex flex-col justify-between px-3 md:py-5 text-lg gap-2">
                 <div className={`text-gray-300 flex items-center gap-2`}>
                     <div className={`${bgColor} rounded-lg p-2`}>
                         <img src={icon} className="w-6" />
@@ -21,7 +21,7 @@ export const TotalCards = ({ title, value, bgColor, icon, borderColor }: Props) 
                     </div>
                 </div>
                 <div
-                    className={`text-3xl place-self-end ${(Number(value) < 0) ? "text-red-600" : ""} 
+                    className={`md:text-3xl place-self-end ${(Number(value) < 0) ? "text-red-600" : ""} 
                                 ${title === "Despesas" ? "text-red-500" : "text-green-500"}`}>
                     € {Number(value).toFixed(2)}
                 </div>
